270f207e2f7f739779c2d962c95a197d960276f5
Bo Ingram <boingram6@gmail.com>
2019-04-03 21:43:01 -0500

    add a test for a selected item

--- assets/js/components/Item/Item.test.tsx
+++ assets/js/components/Item/Item.test.tsx
@@ -10,10 +10,11 @@ describe('Item', () => {
   const id = 1;
   const name = 'Kokiri Sword';
   const image = 'kokiri-sword';
-  const selected = false;
   const clickHandler = (): void => {};
 
   test('unselected item ', () => {
+    const selected = false;
+
     const testRenderer: ReactTestRenderer = TestRenderer.create(
       <Item
         id={id}
@@ -37,7 +38,39 @@ describe('Item', () => {
     expect(div.props.className).toBe('item');
 
     const img: ReactTestInstance = div.findByType('img');
-    expect(img.props.className).not.toBe('selected');
+    expect(img.props.className).toBe('unselected');
+    expect(img.props.src).toBe(`images/items/${image}.png`);
+    expect(img.props.onClick).toBeTruthy();
+    expect(img.props.alt).toBe(name);
+  });
+
+  test('selected item ', () => {
+    const selected = true;
+
+    const testRenderer: ReactTestRenderer = TestRenderer.create(
+      <Item
+        id={id}
+        name={name}
+        image={image}
+        selected={selected}
+        clickHandler={clickHandler}
+      />
+    );
+
+    const instance: ReactTestInstance = testRenderer.root;
+    const item: ReactTestInstance = instance.findByType(Item);
+    expect(item.props.id).toBe(id);
+    expect(item.props.name).toBe(name);
+    expect(item.props.image).toBe(image);
+    expect(item.props.selected).toBe(selected);
+    expect(item.props.clickHandler).toBe(clickHandler);
+    expect(item.children).toHaveLength(1);
+
+    const div: ReactTestInstance = item.findByType('div');
+    expect(div.props.className).toBe('item');
+
+    const img: ReactTestInstance = div.findByType('img');
+    expect(img.props.className).toBe('');
     expect(img.props.src).toBe(`images/items/${image}.png`);
     expect(img.props.onClick).toBeTruthy();
     expect(img.props.alt).toBe(name);
