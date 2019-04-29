70ab5a7b4cadb50d81aa36875a49564d8b43d413
Bo Ingram <boingram6@gmail.com>
2019-04-29 17:19:47 -0500

    Build visited items in the state resolver

--- lib/goron_web/resolvers/state_resolver.ex
+++ lib/goron_web/resolvers/state_resolver.ex
@@ -1,5 +1,19 @@
 defmodule GoronWeb.Resolvers.StateResolver do
-  alias Goron.State
+  alias Goron.Item
+  alias Goron.Item.VisitedItem
+
+  def update_item(_parent, %{item_id: item_id, level: level}, _resolution) do
+    item_map =
+      Item.get_all_items()
+      |> Enum.map(&VisitedItem.to_visited_item/1)
+      |> Enum.into(%{}, fn %VisitedItem{} = item -> {item.id, item} end)
+
+    # update the value in the map
+
+    IO.inspect(item_map)
+
+    {:ok, %{}}
+  end
 
   def update_item(_parent, args, _resolution) do
     IO.inspect(args)
