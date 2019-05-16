07c62b7f297ccb9157f7d4af3f70a70a70d0a17b
Bo Ingram <boingram6@gmail.com>
2019-05-15 20:14:49 -0500

    Clean up obsoleted files

--- assets/js/components/Main.tsx
+++ assets/js/components/Main.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import ApolloContext from '../context';
 import App from '../containers/App/App';
 import goronClient from '../api/client/goronClient';
-import ItemPanel from '../containers/ItemPanel/ItemPanel';
 
 const Main: React.FC = (): React.ReactElement => (
   <main role="main" className="container">
